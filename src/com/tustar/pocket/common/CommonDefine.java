4f88f36757e2abb3c2b4e5673d9f01dee6239165
tustar <tustar1030@126.com>
2015-05-06 23:26:30 +0800

    整理常量

--- src/com/tustar/pocket/common/CommonDefine.java
+++ src/com/tustar/pocket/common/CommonDefine.java
@@ -4,14 +4,20 @@ public interface CommonDefine {
 
 	/**
 	 * SharePreferences
+	 * 
+	 * @author tustar
+	 * 
 	 */
 	public interface PrefKey {
-		public static final String APP_PREF_FILE = "wshop";
+		public static final String APP_PREF_FILE = "pocket";
 		public static final String PREF_KEY_IMEI = "imei";
 	}
 
 	/**
 	 * Meta-data name
+	 * 
+	 * @author tustar
+	 * 
 	 */
 	public interface MetaDataName {
 		public static final String META_NAME_UMENG_CHANNEL = "UMENG_CHANNEL";
@@ -19,6 +25,9 @@ public interface CommonDefine {
 
 	/**
 	 * Intent key
+	 * 
+	 * @author tustar
+	 * 
 	 */
 	public interface IntentKey {
 		public static final String INTENT_KEY_PUSH_MSG = "push_msg";
@@ -26,6 +35,9 @@ public interface CommonDefine {
 
 	/**
 	 * Intent action
+	 * 
+	 * @author tustar
+	 * 
 	 */
 	public interface IntentAction {
 		public static final String INTENT_ACTION_SHOW_GETUI_PUSH = "show_getui_push";
@@ -33,10 +45,23 @@ public interface CommonDefine {
 
 	/**
 	 * Umeng key
+	 * 
+	 * @author tustar
+	 * 
 	 */
 	public interface UmengEvent {
 		public static final String UMENG_KEY_CLICK_CHAT = "click_chat";
 		public static final String UMENG_KEY_CLICK_IMAGE = "click_image";
 		public static final String UMENG_KEY_CLICK_EXIT_OWNER = "click_exit_owner";
+	}
+
+	/**
+	 * Request code
+	 * 
+	 * @author tustar
+	 * 
+	 */
+	public interface RequestCode {
+		public static final int MainActivity = 0x00001;
 	}
 }
